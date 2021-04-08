5ea980555fbd6223ce5c1888e06965938ea7a0e5
zqh <1039903166@qq.com>
2021-04-09 06:28:50 +0800

    solutions had been classified

---
+++ solution/commonFun.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+
+using namespace std;
+
+//int max(int a, int b)
+//{
+//  return a > b ? a : b;
+//}
